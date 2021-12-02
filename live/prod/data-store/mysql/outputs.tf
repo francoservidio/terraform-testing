f5027cac3c9fb41a86c109f90d00518236a8bf15
fservidioo <fservidioo@gmail.com>
2021-12-02 11:51:59 -0300

    small modules

--- live/prod/data-store/mysql/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "address" {
-  value = aws_db_instance.prod-example.address
-  description = "endpoint of the database"
-}
-output "port" {
-  value = aws_db_instance.prod-example.port
-  description = "the port the database is listening on"
-}
-
--- live/stage/data-store/mysql/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "address" {
-  value = aws_db_instance.example.address
-  description = "endpoint of the database"
-}
-output "port" {
-  value = aws_db_instance.example.port
-  description = "the port the database is listening on"
-}
-
